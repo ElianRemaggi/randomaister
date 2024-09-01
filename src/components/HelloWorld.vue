876b7707fd09b6420a96a7011cb72193bdc62c17
elian <elianremaggi@gmail.com>
2024-09-01 20:13:12 -0300

    Clean base proyect

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,14 @@
-<script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
-</script>
-
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
   </div>
 </template>
+
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
 
 <style scoped>
 h1 {
@@ -25,6 +25,7 @@ h1 {
 }
 
 @media (min-width: 1024px) {
+
   .greetings h1,
   .greetings h3 {
     text-align: left;
